f2fc35d3f96d57304fc24f313a8b9b93851ce552
alongkornubu <alongkorn.ch.62@ubu.ac.th>
2022-02-04 21:08:41 +0700

    one

---
+++ secproj/src/app/app-routing.module.ts
@@ -0,0 +1,38 @@
+import { NgModule } from '@angular/core';
+import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
+
+const routes: Routes = [
+  {
+    path: 'home',
+    loadChildren: () => import('./home/home.module').then( m => m.HomePageModule)
+  },
+  {
+    path: '',
+    redirectTo: 'dbhome',
+    pathMatch: 'full'
+  },
+  {
+    path: 'login',
+    loadChildren: () => import('./login/login.module').then( m => m.LoginPageModule)
+  },
+  {
+    path: 'avatarpg',
+    loadChildren: () => import('./avatarpg/avatarpg.module').then( m => m.AvatarpgPageModule)
+  },
+  {
+    path: 'avatarresult/:sendobj',
+    loadChildren: () => import('./avatarresult/avatarresult.module').then( m => m.AvatarresultPageModule)
+  },
+  {
+    path: 'dbhome',
+    loadChildren: () => import('./dbhome/dbhome.module').then( m => m.DbhomePageModule)
+  },
+];
+
+@NgModule({
+  imports: [
+    RouterModule.forRoot(routes, { preloadingStrategy: PreloadAllModules })
+  ],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
